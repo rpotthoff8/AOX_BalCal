c427c6c91df24440c680912f7645207d02b8d997
jv11-rice <44072631+jv11-rice@users.noreply.github.com>
2018-12-05 18:54:50 -0600

    fixed bug in meantare.m

--- meantare.m
+++ meantare.m
@@ -1,6 +1,7 @@
 function taretal = meantare(series,checkit)
 % SOLVE TARES BY TAKING THE MEAN
-[~,~,s_id] = unique(series);
+[s,~,s_id] = unique(series);
+nseries = length(s);
 for i = 1:nseries
     zoop = checkit(s_id==i,:);
     zap(i,:) = mean(zoop);
