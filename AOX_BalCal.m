7b929d08348ed99716359a28ac18aec5802dfb2f
rpotthoff8 <44843703+rpotthoff8@users.noreply.github.com>
2018-12-12 12:50:14 -0600

    comLZ matrix change
    removed row of 1s at bottom in loop

--- AOX_BalCal.m
+++ AOX_BalCal.m
@@ -2238,10 +2238,10 @@ for i=1:lasttare
 end
 
 %
-
-for loopk=1:numpts
-    comLZ(nterms+series(loopk),loopk) = 1.0;
-end
+%Change!!
+% for loopk=1:numpts
+%     comLZ(nterms+series(loopk),loopk) = 1.0;
+% end
 
 %%
 %%
