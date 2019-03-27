3a7dd340d1c68ba97b7dcdf06548563df33d3ed1
rpotthoff8 <44843703+rpotthoff8@users.noreply.github.com>
2019-03-27 16:15:16 -0500

    Create uncert_prop.m

---
+++ uncert_prop.m
@@ -0,0 +1,84 @@
+function [combined_uncert,tare_uncert, FL_uncert]=uncert_prop(xcalib,fxcalib_ci,comIN,dimFlag,uncert_comIN,indexLocalZero,lasttare,nterms,aprxIN,series,voltTrust,Boot_Flag,Volt_Flag)
+%Function calculates uncertainty in load from uncertainty in coefficients and input voltages
+
+%Inputs:
+%xcalib: Coefficients from calibration
+%fxcalib_ci: Confidence interval for coefficients
+%comIN: Produced by balCal_algEquations3: Voltages provided put into matrix
+%to multiply by coefficients for load output
+%dimFlag: Number of dimensions (channels)
+%uncert_comIN: Produced by balCal_algEquations3: Voltages provided put into
+%matrix to multiply by coefficients for partial derivatives
+%indexLocalZero: Index of where which datapoints are tare loads
+%lasttare: Number of the final series
+%nterms: number of coefficients used for model
+%aprxIN: approximation matrix of loads
+%voltTrust: 95% confidence (+/-) for voltage readings (in mV)
+%Boot_Flag: 1 or 0: flag for if bootstrap method was used to find coeff CI
+%Volt_Flag: 1 or 0: If voltage uncertainty prop is to be performed
+
+%Outputs:
+%combined_uncert: Uncertainty (in pounds) due to coefficient and voltage uncertainty
+%for each datapoint
+%tare_uncert: Uncertainty (in pounds) due to coefficient and voltage uncertainty
+%for tare loads only
+%FL_uncert: Uncertainty for loads of (calculated-tare): combines
+%uncertainty in calculated loads and tare loads
+
+%START: coeff uncertainty propagation JRP 16 Jan 19
+%Take Confidence interval found for coefficients using bootstrap, and store
+%the absolute value of the larger error between +/- in a matrix
+if Boot_Flag==1
+for i=1:size(xcalib,1)
+    for j=1:size(xcalib,2)
+        for k=1:2
+            error(k,i,j)=abs(fxcalib_ci(k,i,j)-xcalib(i,j));
+        end
+        xcalib_error(i,j)=max(error(:,i,j));
+    end
+end
+else
+    xcalib_error=zeros(size(xcalib,1),size(xcalib,2));
+end
+comIN_square=comIN.^2; %Square input matrix
+xcalib_error_square=xcalib_error.^2; %square error matrix
+coeff_uncert_square=(xcalib_error_square'*comIN_square)'; %Matrix for error in each calibration point from uncertainty in coefficients(+/-)
+%END:  coeff uncertainty propagation
+
+%ADDED 23 Jan 19 JRP: Analytical calc of uncert in loads due to uncertainty in read
+%voltages
+if Volt_Flag==1
+uncert=voltTrust; %Trust all channels down to 1 microvolt
+for i=1:lasttare
+    for j=1:dimFlag
+        uncert_comIN(nterms+i,:,j) = 0;
+    end
+end
+uncert_comIN_use=uncert_comIN(1:size(xcalib,1),:,:);
+volt_uncert_square=zeros(size(aprxIN));
+for i=1:dimFlag
+    partial(:,:,i)=uncert_comIN_use(:,:,i)'*xcalib;
+    uncert_channel_square(:,:,i)=((partial(:,:,i)).^2).*uncert^2;
+    volt_uncert_square=volt_uncert_square+(uncert_channel_square(:,:,i));
+end
+else
+    volt_uncert_square=zeros(size(aprxIN));
+end
+%Combine uncertainty from coeff and input voltages for 1 total uncertainty
+%value for every datapoint:
+combined_uncert=(coeff_uncert_square+volt_uncert_square).^(.5);
+tare_uncert=combined_uncert(indexLocalZero(1:(numel(indexLocalZero)-1)),:); %Uncert for tare loads
+
+%FL Uncert:
+for i=1:numel(series)
+    if i==indexLocalZero(series(i))
+        FL_uncert(i,:)=combined_uncert(i,:);
+    else
+        FL_uncert(i,:)=(combined_uncert(i,:).^2+tare_uncert(series(i)).^2).^.5;
+    end
+
+end
+
+%END added for uncert
+
+end
