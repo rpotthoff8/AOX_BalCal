dac258e20967315fef52197110bfc2d053ec7487
rpotthoff8 <44843703+rpotthoff8@users.noreply.github.com>
2019-03-27 15:11:36 -0500

    add function to calc xcalib

--- AOX_BalCal.m
+++ AOX_BalCal.m
@@ -95,6 +95,8 @@ nseries0 = length(s_1st0);
 if model_FLAG == 4
     customMatrix = out.customMatrix;
     customMatrix = [customMatrix; ones(nseries0,dimFlag)];
+else 
+    customMatrix = [];
 end
 
 % Load data labels if present, otherwise use default values.
@@ -174,42 +176,9 @@ for lhs = 1:numLHS
     series = series0(sample);
     targetMatrix = targetMatrix0(sample,:);
     comIN = comIN0(sample,:);
-    
-    % Characterizes the series in the subsamples
-    [~,s_1st,~] = unique(series);
-    nseries = length(s_1st);
-    
     fprintf('\nWorking ...\n')
-    % Indirect approach uses the modeled voltage
-%     if approach_FLAG == 1
-%         if balCal_FLAG == 2
-%             excessVec0 = qtaprxINminGZ2 + globalZerosAllPoints;
-%         else
-%             excessVec0 = qtaprxINminGZ + globalZerosAllPoints;
-%         end
-%     end  
-
-    xcalib = zeros(nterms+nseries0,dimFlag);
-    % Solves for the coefficient one column at a time.
-    % This is to account for Custom Models, where the terms may be
-    % different depending on the channel.
-    for k = 1:dimFlag
-        comIN_k = comIN;
-        
-        if model_FLAG == 4
-            comIN_k(:,customMatrix(:,k)==0) = [];
-        end
-        
-        % SOLUTION
-        xcalib_k = pinv(comIN_k)*targetMatrix(:,k);
-        
-        if model_FLAG == 4
-            xcalib(customMatrix(:,k)==1,k) = xcalib_k;
-        else
-            xcalib(:,k) = xcalib_k;
-        end
-        
-    end
+   
+    xcalib=calc_xcalib(comIN,series,nterms,nseries0,dimFlag,model_FLAG,targetMatrix,customMatrix);
     
     if LHS_FLAG == 1
         x_all(:,:,lhs) = xcalib;
---
+++ calc_xcalib.m
@@ -0,0 +1,36 @@
+function xcalib=calc_xcalib(comIN,series,nterms,nseries0,dimFlag,model_FLAG,targetMatrix,customMatrix)
+%Function calculates coefficient matrix (xcalib)
+
+% Characterizes the series in the subsamples
+    [~,s_1st,~] = unique(series);
+    nseries = length(s_1st);
+
+    % Indirect approach uses the modeled voltage
+%     if approach_FLAG == 1
+%         if balCal_FLAG == 2
+%             excessVec0 = qtaprxINminGZ2 + globalZerosAllPoints;
+%         else
+%             excessVec0 = qtaprxINminGZ + globalZerosAllPoints;
+%         end
+%     end  
+
+    xcalib = zeros(nterms+nseries0,dimFlag);
+    % Solves for the coefficient one column at a time.
+    % This is to account for Custom Models, where the terms may be
+    % different depending on the channel.
+    for k = 1:dimFlag
+        comIN_k = comIN;
+        
+        if model_FLAG == 4
+            comIN_k(:,customMatrix(:,k)==0) = [];
+        end
+        
+        % SOLUTION
+        xcalib_k = pinv(comIN_k)*targetMatrix(:,k);
+        
+        if model_FLAG == 4
+            xcalib(customMatrix(:,k)==1,k) = xcalib_k;
+        else
+            xcalib(:,k) = xcalib_k;
+        end
+    end
