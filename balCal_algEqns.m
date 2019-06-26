0319ed90def87acafa724bdac4f9e01434b956cb
jv11-rice <44072631+jv11-rice@users.noreply.github.com>
2019-06-26 18:56:26 -0500

    Added flag to normalize data during calibration

--- balCal_algEqns.m
+++ balCal_algEqns.m
@@ -1,29 +1,41 @@
 % Copyright ©2017 Andrew Meade, Javier Villarreal.  All Rights Reserved.
-function in_comb = balCal_algEqns(model_FLAG,in,series)
+function in_comb = balCal_algEqns(model_FLAG,in,series,normFLAG)
+
+if nargin <4
+    normFLAG = 0;
+end
+
+if normFLAG == 1
+    range = max(in) - min(in);
+    shift = min(in) + range/2;
+    in_n = (in - shift)./(range/2);
+else
+    in_n = in;
+end
 
 % Detect the size of the input
-n = size(in,1); %number of data points
-d = size(in,2); %data dimensionality.
+n = size(in_n,1); %number of data points
+d = size(in_n,2); %data dimensionality.
 if model_FLAG == 3
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%LINEAR MODEL
-    in_comb = in;%                                                        3
+    in_comb = in_n;%                                                        3
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     in_comb = interceptTerms(in_comb,series);
     return 
 end
 
-in_sq = in.^2;
+in_sq = in_n.^2;
 j = 1;
 ini_inj = zeros(n,(d^2-d)/2);
 for k = 1:d-1
     for m = k+1:d
-        ini_inj(:,j) = in(:,k).*in(:,m);
+        ini_inj(:,j) = in_n(:,k).*in_n(:,m);
         j = j+1;
     end
 end
 if model_FLAG == 2
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%TRUNCATED MODEL
-    in_comb = [in,      ...                                               2
+    in_comb = [in_n,      ...                                               2
                in_sq,   ...
                ini_inj];
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -31,23 +43,30 @@ if model_FLAG == 2
     return 
 end
 
+
 abs_in = abs(in);
-in_absin = in.*abs_in;
+if normFLAG == 1
+    range = max(abs_in) - min(abs_in);
+    shift = min(abs_in) + range/2;
+    abs_in = (abs_in - shift)./(range/2);
+end
+    
+in_absin = in_n.*abs_in;
 abs_iniinj = abs(ini_inj);
 j = 1;
 ini_absinj = zeros(n,(d^2-d)/2);
 absini_inj = zeros(n,(d^2-d)/2);
 for k = 1:d-1
     for m = k+1:d
-        ini_absinj(:,j) = in(:,k).*abs_in(:,m);
-        absini_inj(:,j) = abs_in(:,k).*in(:,m);
+        ini_absinj(:,j) = in_n(:,k).*abs_in(:,m);
+        absini_inj(:,j) = abs_in(:,k).*in_n(:,m);
         j = j+1;
     end
 end
-in_cu = in.^3;
+in_cu = in_n.^3;
 abs_incu = abs(in_cu);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%FULL MODEL
-in_comb = [in,        ...                                                 1
+in_comb = [in_n,        ...                                                 1
            abs_in,    ...
            in_sq,     ...
            in_absin,  ...
